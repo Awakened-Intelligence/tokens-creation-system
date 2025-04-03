b9846556818739af6a829f6b64cb5310a9b96154
talhailyasai <talhailyasai@gmail.com>
2025-04-04 04:03:17 +0500

    frontend/src/config.js domain name changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config={
     // API_BASE_URL:"http://127.0.0.1:5000"
-    API_BASE_URL:"https://ec2-18-212-218-211.compute-1.amazonaws.com"
+    API_BASE_URL:"http://18.212.218.211"
 };
 
 export default config;

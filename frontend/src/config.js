c0f9effba175560b9e92eba42c6ee08420e2122f
talhailyasai <talhailyasai@gmail.com>
2025-04-07 21:39:42 +0500

    Your commit message here

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,5 @@
 const config={
-    // API_BASE_URL:"http://127.0.0.1:5000"
-    API_BASE_URL:"http://18.212.218.211"
+    API_BASE_URL:"https://token.hub.fullpotential.com"
 };
 
 export default config;
